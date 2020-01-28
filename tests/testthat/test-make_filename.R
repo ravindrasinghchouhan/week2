fc7032d2af5e6ea6664b139742b9a4382f49a620
ravindrasinghchouhan <54267422+ravindrasinghchouhan@users.noreply.github.com>
2020-01-28 10:23:57 +0530

    update

--- tests/testthat/test-make_filename.R
+++ tests/testthat/test-make_filename.R
@@ -1,5 +1,7 @@
+library(testthat)
+library(week2)
 test_that('check filename is created properly', {
-  filename_2013 <- make_filename(2013)
+  filename_2013 <- week2::make_filename(2013)
 
   expect_that(filename_2013, is_a('character'))
 })
